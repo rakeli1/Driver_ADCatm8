f14b43c2d83f0c5d47d1fb14a9f9ebc1c9aefed8
Краснобаев Алексей <rakeliit@protonmail.com>
2026-08-01 16:50:40 +0300

    Заполнил README в папке ADC_Basic_Test и добавил файл main.c

---
+++ examples/ADC_Basic_Test/main.c
@@ -0,0 +1,32 @@
+/*
+ * Driver_ADCatm8.c
+ *
+ * Created: 27.07.2026 21:39:49
+ * Author : rakel
+ */ 
+
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include <stdint.h>
+#include "Driver_ADCatm8.h"
+
+
+ADC_Config config = {
+	.conversionMode = ADC_SINGLE_CONVERSION,
+	.reference      = ADC_REF_INTERNAL,
+	.prescaler      = ADC_PRECALER_64,
+	.resultAlign    = ADC_ALIGN_RIGHT
+};
+
+
+int main(void)
+{
+	adc_Init(&config);
+    
+    while (1) 
+    {
+        adc_Read(ADC_CHANNEL_5);
+		
+    }
+}
+
